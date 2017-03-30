e4096e78a9d64db811859f6a0fdeb31f6a79f9d5
rajukan <gyan.raju@gmail.com>
2017-03-30 13:17:38 -0400

    Update Client.java

--- src/com/vg/lifecycle/Client.java
+++ src/com/vg/lifecycle/Client.java
@@ -6,7 +6,6 @@ import org.springframework.context.support.FileSystemXmlApplicationContext;
 public  class Client {
 	
 	public static void main(String[] args) {
-		String  projectPath ="\\/media/gyanu/New Volume/eclipseworkspaces/ws1/spring_hello/META-INF/resources";
 		
 		ApplicationContext fndCtx = new FileSystemXmlApplicationContext( 
 				"/META-INF/resources/03-lifecycle/spring-bootstrap.xml");
